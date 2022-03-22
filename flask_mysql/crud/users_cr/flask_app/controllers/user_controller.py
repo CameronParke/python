a6b7f4be8789cd36ebe70bc1ebbb2016855df63b
CameronParke <campaign711@gmail.com>
2022-03-22 03:09:53 -0500

    Users CRUD Modularized

---
+++ flask_mysql/crud/users_cr/flask_app/controllers/user_controller.py
@@ -0,0 +1,62 @@
+from flask_app import app
+from flask import render_template, redirect, request
+from flask_app.models.user import User
+
+@app.route('/')
+def index():
+    return redirect('/users')
+
+@app.route('/users')
+def users():
+    return render_template("read.html", users=User.get_all())
+
+@app.route('/users/create')
+def create():
+    return render_template("create.html")
+
+
+@app.route('/user/add', methods=['POST'])
+def add_user():
+    print(request.form)
+    User.save(request.form)
+    # data = { 
+    #     "first_name" : request.form["first_name"],
+    #     "last_name" : request.form["last_name"],
+    #     "email" : request.form["email"],
+    #     "created_at" : request.form["created_at"]
+    # }
+    # new_user_id = User.add_new_user(data)
+    return redirect("/users")
+
+
+
+@app.route('/user/<int:id>')
+def show_user(id):
+    data = {
+        "id" : id
+    }
+    # one_friend storing the singular instance we created, now stored in one friend
+# to send stuff from the server to the HTML need to include it after the render_template
+    return render_template("show_user.html", user = User.get_one_user(data))
+
+@app.route('/user/edit/<int:id>')
+def edit_user(id):
+    data = {
+        "id" : id
+    }
+# one_friend storing the singular instance we created, now stored in one friend
+# to send stuff from the server to the HTML need to include it after the render_template
+    return render_template("edit_user.html", user = User.get_one_user(data))
+
+@app.route('/user/edit', methods=['POST'])
+def update_user():
+    User.update_user(request.form)   
+    return redirect('/users')
+
+@app.route('/user/delete/<int:id>')
+def delete_user(id):
+    data = {
+        "id" : id
+    }
+    User.delete_user(data)
+    return redirect('/users')
